f228bb894ebc563070380f90b94f1b6cbc35ffc0
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-07-03 13:00:16 +0200

    Ajoutez des fichiers projet.

---
+++ Unowhy Tools Lite/Main.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Unowhy_Tools_Lite
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.bios = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(48, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIOS Version :";
+            // 
+            // bios
+            // 
+            this.bios.AutoSize = true;
+            this.bios.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bios.ForeColor = System.Drawing.Color.White;
+            this.bios.Location = new System.Drawing.Point(50, 36);
+            this.bios.Name = "bios";
+            this.bios.Size = new System.Drawing.Size(35, 15);
+            this.bios.TabIndex = 1;
+            this.bios.Text = "BIOS";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Unowhy_Tools_Lite.Properties.Resources.ic;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(48, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Password :";
+            // 
+            // pass
+            // 
+            this.pass.AutoSize = true;
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.ForeColor = System.Drawing.Color.White;
+            this.pass.Location = new System.Drawing.Point(50, 85);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(38, 15);
+            this.pass.TabIndex = 1;
+            this.pass.Text = "PASS";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Unowhy_Tools_Lite.Properties.Resources.key;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Main
+            // 
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(507, 123);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bios);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Unowhy Tools Lite";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label bios;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label pass;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Unowhy Tools Lite/Main.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace Unowhy_Tools_Lite
+{
+    public partial class Main : Form
+    {
+        [DllImport("DwmApi")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+                DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+        }
+
+        string GetLine(string text, int lineNo)
+        {
+            string[] lines = text.Replace("\r", "").Split('\n');
+            return lines.Length >= lineNo ? lines[lineNo - 1] : null;
+        }
+
+        public Main()
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = "wmic";
+            p.StartInfo.Arguments = "bios get smbiosbiosversion";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+            
+            string enestr = p.StandardOutput.ReadToEnd();
+            string enestr2 = GetLine(enestr, 2);
+
+            InitializeComponent();
+
+            
+
+            bios.Text = enestr2;
+
+            if (bios.Text.Contains("ENE_"))
+            {
+                if (bios.Text.Contains("ENE_1"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene1.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("ENE_2"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene2.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("ENE_3"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene3.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("ENE_4"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene4.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("ENE_5"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene5.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("ENE_6"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene6.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("ENE_7"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene7.ToString();
+                    pass.Text = passstr;
+                }
+            }
+            else
+            {
+                if (bios.Text.Contains("timing"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene.ToString();
+                    pass.Text = passstr;
+                }
+
+                if (bios.Text.Contains("_ENE"))
+                {
+                    string passstr = Unowhy_Tools_Lite.Properties.Resources.ene.ToString();
+                    pass.Text = passstr;
+                }
+            }
+
+            if(pass.Text == "PASS")
+            {
+                pass.Text = "Only for Y13 / Seulement pour le Y13";
+            }
+        }
+    }
+}
